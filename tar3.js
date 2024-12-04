684f08a212c8f15bc9be7e0d55fc53ad9812b310
Avivd121 <130778737+Avivd121@users.noreply.github.com>
2024-12-04 17:15:13 +0200

    Update tar3.js

--- tar3.js
+++ tar3.js
@@ -1,3 +1,9 @@
+//Presenter: Aviv Dahan  
+// The program counts the number of zeros in an array of numbers.
+// It iterates through each element in the array, checks if the element is zero, 
+// and increments the counter if true.
+// Finally, the program prints the total count of zeros found in the array.
+
 const num = [4, 0, 2, 0, 5, 7, 0, 2, 0];
 let count = 0;
 
